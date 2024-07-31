0278b359344a639571192e3108e9392a2acf87c9
Himanshu Kumar <imhimanshu369@gmail.com>
2024-07-31 07:31:41 +0530

    Added test

---
+++ src/lib.rs
@@ -0,0 +1,58 @@
+use num_bigint::BigUint;
+
+// output = n^exp mod p
+pub fn exponentiate(n: &BigUint, exponent: &BigUint, modulus: &BigUint)-> BigUint{
+    n.modpow(exponent, modulus)
+}
+
+// output = s = k - c * x mod q 
+pub fn solve(k: &BigUint, c: &BigUint, x: &BigUint, q: &BigUint)-> BigUint{
+    if *k >= c * x{
+        return  (k - c * x).modpow(&BigUint::from(1u32), q);
+    }
+    return  q - (c * x - k).modpow(&BigUint::from(1u32), q);
+}
+
+// cond1 : r1 = alpha^s * y1^c
+// cond2 : r2 = beta^s * y2^c
+pub fn verify(r1: &BigUint, r2: &BigUint, y1: &BigUint, y2: &BigUint, alpha: &BigUint, beta: &BigUint, c: &BigUint, s: &BigUint, p: &BigUint )-> bool{
+    let cond1: bool = *r1 == (alpha.modpow(s, p) * y1.modpow(c, p)).modpow(&BigUint::from(1u32), &p);
+    let cond2: bool = *r2 == (beta.modpow(s, p) * y2.modpow(c, p)).modpow(&BigUint::from(1u32), &p);
+    cond1 && cond2
+}
+
+#[cfg(test)]
+mod test {
+
+    use super::*;
+
+    #[test]
+    fn test_toy_example(){
+        let alpha = BigUint::from(4u32);
+        let beta = BigUint::from(9u32);
+        let p: BigUint = BigUint::from(23u32);
+        let q: BigUint = BigUint::from(11u32);
+
+        let x: BigUint = BigUint::from(6u32);
+        let k: BigUint = BigUint::from(7u32);
+
+        let c: BigUint = BigUint::from(4u32);
+
+        let y1: BigUint = exponentiate(&alpha, &x, &p);
+        let y2: BigUint = exponentiate(&beta, &x, &p);
+        assert_eq!(y1, BigUint::from(2u32));
+        assert_eq!(y2, BigUint::from(3u32));
+
+        let r1: BigUint = exponentiate(&alpha, &k, &p);
+        let r2: BigUint = exponentiate(&beta, &k, &p);
+        assert_eq!(r1, BigUint::from(8u32));
+        assert_eq!(r2, BigUint::from(4u32));
+
+        let s: BigUint = solve(&k, &c, &x, &q);
+        assert_eq!(s, BigUint::from(5u32));
+
+        let result: bool = verify(&r1, &r2, &y1, &y2, &alpha, &beta, &c, &s, &p);
+        assert!(result);
+
+    }
+}
